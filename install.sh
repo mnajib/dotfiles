fe953308daa3dcebd7d640b9bebea53146579efc
Mohd Najib Bin Ibrahim <m.najib@gmail.com>
2013-12-12 12:50:46 +0800

    change to suit me

--- install.sh
+++ install.sh
@@ -5,19 +5,19 @@ git submodule init
 git submodule update
 
 echo "Deleting the old files"
-rm ~/.vimrc
 rm ~/.zshrc
-rm ~/.vim
-rm ~/.gvimrc
+#rm ~/.vimrc
+#rm ~/.vim
+#rm ~/.gvimrc
 rm ~/.gitconfig
 rm ~/.gitignore
 rm ~/.tmux.conf
 
 echo "Symlinking files"
-ln -s ~/Github/dotfiles/vimrc ~/.vimrc
 ln -s ~/Github/dotfiles/zshrc ~/.zshrc
-ln -s ~/Github/dotfiles/vim ~/.vim
-ln -s ~/Github/dotfiles/gvimrc ~/.gvimrc
+#ln -s ~/Github/dotfiles/vimrc ~/.vimrc
+#ln -s ~/Github/dotfiles/vim ~/.vim
+#ln -s ~/Github/dotfiles/gvimrc ~/.gvimrc
 ln -s ~/Github/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/Github/dotfiles/gitignore ~/.gitignore
 ln -s ~/Github/dotfiles/tmux ~/.tmux.conf
@@ -27,4 +27,3 @@ echo "Updating submodules"
 git submodule foreach git pull origin master --recurse-submodules
 
 echo "All done."
-
